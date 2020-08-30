1724d6a700a9d5f3c4b2e54a3fe3cbadb20b3973
adarsh189-pal <64680183+adarsh189-pal@users.noreply.github.com>
2020-08-30 22:21:30 +0530

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -8,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.light,
         primaryColor: Colors.blue,
@@ -24,63 +29,164 @@ class MyAppPage extends StatefulWidget {
 }
 
 class _MyAppPageState extends State<MyAppPage> {
+  String studentName, systemId, studyProgramId;
+  double studentGpa;
+
+  getStudentName(name) {
+    this.studentName = name;
+  }
+
+  getSystemId(id) {
+    this.systemId = id;
+  }
+
+  getStudyProgramId(programId) {
+    this.studyProgramId = programId;
+  }
+
+  getStudentGpa(gpa) {
+    this.studentGpa = double.parse(gpa);
+  }
+
+  createData() {
+    print('created');
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection('MyStudent').doc(studentName);
+    Map<String, dynamic> students = {
+      'studentName': studentName,
+      'studentId': systemId,
+      'studyProgramId': studyProgramId,
+      'studentGpa': studentGpa
+    };
+    documentReference.set(students).whenComplete(() {
+      print('$studentName created');
+    });
+  }
+
+  readData() {
+    print('read');
+  }
+
+  updateData() {
+    print('updated');
+  }
+
+  deleteData() {
+    print('deleted');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Firebase College'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Name',
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue, width: 2),
-                  )),
-              onChanged: (String name) {},
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Name',
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                    )),
+                onChanged: (String name) {
+                  getStudentName(name);
+                },
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Student ID',
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue, width: 2),
-                  )),
-              onChanged: (String name) {},
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'System ID',
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                    )),
+                onChanged: (String id) {
+                  getSystemId(id);
+                },
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Students Program',
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue, width: 2),
-                  )),
-              onChanged: (String name) {},
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Study Program',
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                    )),
+                onChanged: (String programId) {
+                  getStudyProgramId(programId);
+                },
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Student GPA',
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue, width: 2),
-                  )),
-              onChanged: (String name) {},
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Student GPA',
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                    )),
+                onChanged: (String gpa) {
+                  getStudentGpa(gpa);
+                },
+              ),
             ),
-          )
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton(
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Text('CREATE'),
+                  textColor: Colors.white,
+                  onPressed: () {
+                    createData();
+                  },
+                ),
+                RaisedButton(
+                  color: Colors.red,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Text('READ'),
+                  textColor: Colors.white,
+                  onPressed: () {
+                    readData();
+                  },
+                ),
+                RaisedButton(
+                  color: Colors.green,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Text('UPDATE'),
+                  textColor: Colors.white,
+                  onPressed: () {
+                    updateData();
+                  },
+                ),
+                RaisedButton(
+                  color: Colors.purple,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Text('DELETE'),
+                  textColor: Colors.white,
+                  onPressed: () {
+                    deleteData();
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
